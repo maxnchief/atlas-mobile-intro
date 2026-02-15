54d0f93fde41405b6994fbcba0e45eeeea22d9a0
maxnchief <logandogan@gmail.com>
2026-02-15 12:13:27 -0600

    Add DatabaseProvider component to manage SQLite database loading

---
+++ components/DatabaseProvider.tsx
@@ -0,0 +1,40 @@
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+import { useEffect, useState } from "react";
+
+async function loadDatabase() {
+  const name = "scores.db";
+  const dbPath = `${FileSystem.documentDirectory}SQLite/${name}`;
+  const fileInfo = await FileSystem.getInfoAsync(dbPath);
+
+  if (!fileInfo.exists) {
+    //Create DB
+    const dbAsset = require("@/assets/" + name);
+    const dbUri = Asset.fromModule(dbAsset).uri;
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true },
+    );
+    await FileSystem.downloadAsync(dbUri, dbPath);
+  }
+}
+
+function useDB() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    loadDatabase().then(() => setLoaded(true));
+  }, []);
+
+  return { loaded };
+}
+
+function DatabaseProvider({ children }) {
+  const { loaded } = useDB();
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <SQLite.SQLiteProvider>{children}</SQLite.SQLiteProvider>;
+}
