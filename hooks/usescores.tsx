98026201d99f3a14c047fc116259a11f88beb472
maxnchief <logandogan@gmail.com>
2026-02-15 13:17:39 -0600

    Implement useScores hook for managing Wordle scores with SQLite

---
+++ hooks/usescores.tsx
@@ -0,0 +1,24 @@
+import { useSQLiteContext } from "expo-sqlite";
+import { use, useEffect, useState } from "react";
+
+type Score = {
+    id: number;
+    score: number;
+    date: string;
+}
+
+export function useScores() {
+    const [wordle_scores, setScores] = useState([]);
+  
+    const db = useSQLiteContext();
+
+  function getScores() {
+    return db.getAllSync<Score>("SELECT * FROM wordle_scores");
+  }
+
+useEffect(() => {
+   const data = getScores():
+    }, []);
+  
+  return { getScores wordle_scores};
+}
